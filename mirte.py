bd1cb7bb1cc38a7520c9770cb070aaae0d2c8bc2
Bas Westerbaan <bas@westerbaan.name>
2010-04-19 16:44:04 +0200

    mirte: implement get_or_create_a
    Signed-off-by: Bas Westerbaan <bas@westerbaan.name>

--- mirte.py
+++ mirte.py
@@ -1,5 +1,7 @@
 from __future__ import with_statement
 
+from itertools import chain, product
+
 import threading
 import optparse
 import logging
@@ -9,6 +11,9 @@ import select
 import yaml
 import sys
 import os
+
+def pick(iterable):
+	return iter(iterable).next()
 
 class Module(object):
 	def __init__(self, settings, logger):
@@ -105,7 +110,7 @@ class InstanceInfo(object):
 		self.module = module
 
 class VSettingDefinition(object):
-	def __init__(self, default=None, _type=None):
+	def __init__(self, _type=None, default=None):
 		self.default = default
 		self.type = _type
 
@@ -120,7 +125,7 @@ class ModuleDefinition(object):
 		self.vsettings = dict() if vsettings is None else vsettings
 		self.implementedBy = implementedBy
 		self.run = run
-		self.inherits = inherits if inherits is None else list()
+		self.inherits = list() if inherits is None else inherits
 	
 	@property
 	def abstract(self):
@@ -139,16 +144,163 @@ class Manager(Module):
 				   'float': float,
 				   'int': int}
 		self.insts = dict()
+		# module -> concrete modules implementing module
+		self.modules_implementing = dict()
+		self.insts_implementing = dict()
 		self._sleep_socketpair = socket.socketpair()
 		self.add_module_definition('module', ModuleDefinition())
 		self.add_module_definition('manager', ModuleDefinition())
-		self.insts['manager'] = InstanceInfo('manager', 'manager',
-						     self, {}, {})
+		self.register_instance('manager', 'manager', self, {}, {})
+
+	def get_all(self, _type):
+		""" Gets all instances implementing type <_type> """
+		if not _type in self.modules:
+			raise ValueError, "No such module, %s" % _type
+		if not self.insts_implementing.get(_type, None):
+			raise ValueError, "No instance implementing %s" % _type
+		return self.insts_implementing[_type]
 	
+	def get_a(self, _type):
+		""" Gets an instance implementing type <_type> """
+		tmp = self.get_all(_type)
+		ret = pick(tmp)
+		if len(tmp) != 1:
+			self.l.warn(("get_a: %s all implement %s; "+
+				     "picking %s") % (tmp, _type, ret))
+		return ret
+
+	def got_a(self, _type):
+		""" Returns whether there is an instance implementing <_type>
+		"""
+		if not _type in self.modules:
+			raise ValueError, "No such module, %s" % _type
+		return (_type in self.insts_implementing and
+			self.insts_implementing[_type])
+
+	class GoCa_Plan(object):
+		""" A partial plan for a get_or_create_a call """
+		def __init__(self, man, targets, insts=None,
+				insts_implementing=None, insts_order=None):
+			self.man = man
+			self.targets = targets
+			self.insts = dict() if insts is None else insts
+			self.insts_implementing = (dict() if insts_implementing
+						is None else insts_implementing)
+			self.insts_order = (list() if insts_order is None
+						else insts_order)
+		def free_instance_name_like(self, name):
+			if (not name in self.insts and
+					not name in self.man.insts):
+				return name
+			suffix = 2
+			while True:
+				shot = "%s-%s" % (name, suffix)
+				if (not shot in self.insts and
+						not name in self.man.insts):
+					return shot
+				suffix += 1
+		def got_a(self, _type):
+			if self.man.got_a(_type):
+				return True
+			return (_type in self.insts_implementing and
+					self.insts_implementing[_type])
+		def get_all(self, _type):
+			ret = list()
+			if self.man.got_a(_type):
+				ret.extend(self.man.get_all(_type))
+			if _type in self.insts_implementing:
+				ret.extend(self.insts_implementing[_type])
+			return ret
+		def get_a(self, _type):
+			return pick(self.get_all(_type))
+		@property
+		def finished(self):
+			return not self.targets
+		def plan_a(self, mod):
+			name = self.free_instance_name_like(mod)
+			self.insts[name] = (name, mod, {})
+			self.insts_order.append(name)
+			md = self.man.modules[mod]
+			for mod2 in chain(md.inherits, (mod,)):
+				if not mod2 in self.insts_implementing:
+					self.insts_implementing[mod2] = list()
+				self.insts_implementing[mod2].append(name)
+			for depName, dep in md.deps.iteritems():
+				if dep.type in self.targets:
+					self.targets[dep.type].append(
+							(name, depName))
+				else:
+					self.targets[dep.type] = [
+							(name,depName)]
+			return name
+		def branches(self):
+			choices = dict()
+			for target in self.targets:
+				if self.got_a(target):
+					choices[target] = [(True, name) for
+						name in self.get_all(target)]
+					continue
+				choices[target] = [(False, name) for name
+					in self.man.modules_implementing[
+						target]]
+			choices_t = choices.items()
+			for choice in product(*[xrange(len(v))
+					for k, v in choices_t]):
+				plan2 = Manager.GoCa_Plan(self.man, dict(),
+						dict(self.insts),
+						dict(self.insts_implementing),
+						list(self.insts_order))
+				tmp = [(choices_t[n][0], choices_t[n][1][m])
+						for n, m in enumerate(choice)]
+				for target, inst_or_mod in tmp:
+					if inst_or_mod[0]:
+						name = inst_or_mod[1]
+					else:
+						name = plan2.plan_a(
+								inst_or_mod[1])
+					for _in, depName in \
+							self.targets[target]:
+						plan2.insts[_in][2][
+								depName] = name
+				yield plan2
+		def execute(self):
+			print (self.insts_order, self.insts,
+				self.insts_implementing, self.targets)
+			for name in reversed(self.insts_order):
+				self.man.create_instance(*self.insts[name])
+
+	def get_or_create_a(self, _type):
+		""" Gets or creates an instance of type <_type> """
+		self.l.debug("get_or_create_a: %s" % _type)
+		stack = [Manager.GoCa_Plan(self, {_type:()})]
+		while stack:
+			p = stack.pop()
+			if p.finished:
+				p.execute()
+				return p.get_a(_type)
+			for c in p.branches():
+				stack.append(c)
+		raise NotImplementedError
+	
+	def free_instance_name_like(self, name):
+		if not name in self.insts: return name
+		suffix = 2
+		while True:
+			shot = "%s-%s" % (name, suffix)
+			if not shot in self.insts:
+				return shot
+			suffix += 1
+
 	def add_module_definition(self, name, definition):
 		if name in self.modules:
 			raise ValueError, "Duplicate module name"
 		self.modules[name] = definition
+		if not definition.implementedBy is None:
+			for t in chain(definition.inherits, (name,)):
+				if not t in self.modules_implementing:
+					self.insts_implementing[t] = set()
+					self.modules_implementing[t] = set()
+				self.modules_implementing[t].add(name)
 	
 	def create_instance(self, name, moduleName, settings):
 		""" Creates an instance of <moduleName> at <name> with
@@ -164,7 +316,8 @@ class Manager(Module):
 		deps = dict()
 		for k, v in md.deps.iteritems():
 			if not k in settings:
-				raise ValueError, "Missing setting %s" % k
+				settings[k] = self.get_or_create_a(
+						v.type)
 			if not settings[k] in self.insts:
 				raise ValueError, "No such instance %s" \
 						% settings[k]
@@ -172,15 +325,25 @@ class Manager(Module):
 			settings[k] = self.insts[settings[k]].object
 		for k, v in md.vsettings.iteritems():
 			if not k in settings:
-				self.l.warn('%s:%s not set' % 
-						(name, k))
-		cl = get_by_path(md.implementedBy)
-		il = logging.getLogger(name)
+				settings[k] = v.default
+				if v.default is None:
+					self.l.warn('%s:%s not set' % 
+							(name, k))
 		self.l.info('create_instance %-15s %s' % (
 				name, md.implementedBy))
+		cl = get_by_path(md.implementedBy)
+		il = logging.getLogger(name)
 		obj = cl(settings, il)
-		self.insts[name] = InstanceInfo(name,moduleName, obj,
+		self.register_instance(name, moduleName, obj, settings, deps)
+	
+	def register_instance(self, name, moduleName, obj, settings, deps):
+		md = self.modules[moduleName]
+		self.insts[name] = InstanceInfo(name, moduleName, obj,
 						settings, deps)
+		for mn in chain(md.inherits, (moduleName,)):
+			if not mn in self.insts_implementing:
+				self.insts_implementing[mn] = set()
+			self.insts_implementing[mn].add(name)
 		if md.run:
 			self.to_stop.append(name)
 			self.daemons.append(name)
@@ -297,7 +460,8 @@ def module_definition_from_mirteFile_dict(man, d):
 			m.deps[k] = DepDefinition(v['type'])
 		elif v['type'] in man.valueTypes:
 			m.vsettings[k] = VSettingDefinition(v['type'],
-				v['default'] if 'default' in v else None)
+				(man.valueTypes[v['type']](v['default'])
+					if 'default' in v else None))
 		else:
 			raise ValueError, \
 				"No such module or valuetype %s" % v
