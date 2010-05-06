4ccfcd64fad7a1741c137db70f4491a9d235d763
Bas Westerbaan <bas@westerbaan.name>
2010-05-06 23:02:19 +0200

    core: add socketServer and unixSocketServer
    socketServer is a generic socket based server
    
    Signed-off-by: Bas Westerbaan <bas@westerbaan.name>

--- core.py
+++ core.py
@@ -1,4 +1,7 @@
 from __future__ import with_statement
+import os
+import socket
+import logging
 import datetime
 import tempfile
 import threading
@@ -382,3 +385,58 @@ class Collection(Module):
 	def by_key(self, key):
 		raise NotImplementedError
 
+class SocketServer(Module):
+	def __init__(self, settings, logger):
+		super(SocketServer, self).__init__(settings, logger)
+		self.lock = threading.Lock()
+		self.running = False
+		self.handlers = set()
+		self.n_conn = 0
+	def create_listening_socket(self):
+		raise NotImplementedError
+	def create_handler(self, con, addr, logger):
+		raise NotImplementedError
+	def run(self):
+		with self.lock:
+			assert not self.running
+			self.running = True
+			self.socket = self.create_listening_socket()
+			self.selectPool.register(self._on_socket_activity,
+					[self.socket])
+	def _on_socket_activity(self, rs, ws, xs):
+		with self.lock:
+			con, addr = self.socket.accept()
+			self.n_conn += 1
+			self.threadPool.execute(self._handle_connection,
+					con, addr, self.n_conn)
+		return True
+	def _handle_connection(self, con, addr, n_conn):
+		l = logging.getLogger("%s.%s" % (self.l.name, n_conn))
+		l.info("Accepted connection from %s" % repr(addr))
+		handler = self.create_handler(con, addr, l)
+		with self.lock:
+			if not self.running:
+				return
+			self.handlers.add(handler)
+		handler.handle()
+		with self.lock:
+			self.handlers.remove(handle)
+		handler.cleanup()
+	def stop(self):
+		with self.lock:
+			assert self.running
+			self.running = False
+			handlers = list(self.handlers)
+		for handler in handlers:
+			handler.interrupt()
+		self.selectPool.deregister([self.socket])
+		self.socket.close()
+
+class UnixSocketServer(SocketServer):
+	def create_listening_socket(self):
+		if os.path.exists(self.socketPath):
+			os.unlink(self.socketPath)
+		s = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+		s.bind(self.socketPath)
+		s.listen(3)
+		return s
