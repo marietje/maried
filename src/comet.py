797241cca1c6cb63a2efca5acca82077fe5ccbac
Bas Westerbaan <bas@westerbaan.name>
2010-06-18 20:49:19 +0200

    comet: some minor progress
    Signed-off-by: Bas Westerbaan <bas@westerbaan.name>

--- src/comet.py
+++ src/comet.py
@@ -1,12 +1,27 @@
 from mirte.core import Module
 from sarah.event import Event
-from sarah.comet import CometServer, BaseCometSession
+from sarah.comet.server import CometServer, BaseCometSession
 
 import time
 import maried
 
 class MariedCometSession(BaseCometSession):
-	pass
+	def handle_message(self, data):
+		if data['type'] == 'get_playing':
+			t = self.server._get_playing()
+			t['type'] = 'playing'
+			self.send_message(t)
+		elif data['type'] == 'list_media':
+			self.send_message({
+				'type': 'media',
+				'media': [m.to_dict() for m
+					in self.server.desk.list_media()]})
+		elif data['type'] == 'list_requests':
+			self.send_message({
+				'type': 'requests',
+				'requests': [r.to_dict() for r
+					in self.server.desk.list_requests()]})
+
 
 class MariedCometServer(CometServer):
 	def __init__(self, settings, logger):
@@ -17,21 +32,19 @@ class MariedCometServer(CometServer):
 		self.desk.on_playing_changed.register(
 				self._on_playing_changed)
 	def _on_media_changed(self):
-		pass
-	def _on_playing_changed(self, previous_playing):
+		self.send_message({'type': 'collection_changed'})
+	def _get_playing(self):
 		playing = self.desk.get_playing()
-		by = None if playing[1] is None else {
-					'key': playing[1].by.key,
-					'realName': playing[1].realName
-					}
+		by = None if playing[1] is None else playing[1].to_dict()
+		media = None if playing[0] is None else playing[0].to_dict()
 		endTime = (time.mktime(playing[2].timetuple()) if
 				not playing[2] is None else None)
-		self.send_message({
-			'type': 'playing_changed',
-			'playing': {
-				'key': playing[0].key,
-				'artist': playing[0].artist,
-				'title': playing[0].title
-			},
-			'endTime': endTime})
+		return {'media': media,
+			'requestedBy': by,
+			'endTime': endTime}
+
+	def _on_playing_changed(self, previous_playing):
+		t = self._get_playing()
+		t['type'] = 'playing_changed'
+		self.send_message(t)
 
